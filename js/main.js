e786ea7e173a8c5f4f9c934e29441cdf189d8735
JxYxxng <ajy9798@gmail.com>
2020-02-27 22:50:02 +0900

    수정

--- js/main.js
+++ js/main.js
@@ -7,10 +7,7 @@ $(function(){
 	$("#gnb li").eq(0).addClass("on");
 
 	setTimeout(function(){
-		$("html").animate({scrollTop:pos}, 800, function(){
-			// $(".container > section").eq(n).addClass("active");
-			// $("#gnb li").eq(n).addClass("on");
-		});
+		$("html").animate({scrollTop:pos}, 800);
 	}, 150);
 
 	$(window).resize(function(){
@@ -26,7 +23,7 @@ $(function(){
 		n=$(this).index();
 		pos=n*h;
 
-		$("html").animate({scrollTop:pos}, 800, function(){
+		$("html").animate({scrollTop:pos}, 700, function(){
 			$(".container > section").removeClass("active");
 			$(".container > section").eq(n).addClass("active");
 			$("#gnb li").removeClass("on");
@@ -66,7 +63,7 @@ $(function(){
 		}
 		pos=n*h;
 
-		$("html").stop().animate({scrollTop:pos}, 700, function(){
+		$("html").stop().animate({scrollTop:pos}, 1000, function(){
 			$(".container > section").removeClass("active");
 			$(".container > section").eq(n).addClass("active");
 			$("#gnb li").removeClass("on");
@@ -74,18 +71,7 @@ $(function(){
 		});
 	});
 
-	// 카카오맵
-	var container = document.getElementById('map');
-	var options = {
-		center: new kakao.maps.LatLng(37.510257, 126.946595), // 중심좌표
-		level: 3 // 지도 크기
-	};
-	var map = new kakao.maps.Map(container, options);
-	var markerPosition  = new kakao.maps.LatLng(37.510257, 126.946595); // 마커 위치
-	var marker = new kakao.maps.Marker({ // 마커 생성
-		position: markerPosition
-	});
-	marker.setMap(map); // 마커 표시
+
 
 	// 포트폴리오
 	$(".portBtn").click(function(e){
@@ -102,42 +88,55 @@ $(function(){
 
 	// 단어
 	var word_list = [
-        {text: "HTML5", weight: 13, url: ""},
-        {text: "jQuery", weight: 10.5, url: "", title: "My Title"},
-        {text: "Parallax", weight: 9.4, url: "javascript:alert('JavaScript in URL is OK!');"},
-        {text: "HTML5", weight: 8},
-        {text: "CSS3", weight: 6.2},
-        {text: "API", weight: 5},
-        {text: "VanillaJS", weight: 5},
-        {text: "SASS", weight: 5},
-        {text: "반응형", weight: 5},
-        {text: "모바일", weight: 4},
-        {text: "PlugIn", weight: 4},
-        {text: "꼼꼼함", weight: 3},
-        {text: "성실함", weight: 3},
-        {text: "책임감", weight: 3},
-        {text: "의사소통", weight: 3},
-        {text: "자기개발", weight: 3},
-        {text: "수학과", weight: 3},
-        {text: "웹디자인기능사", weight: 3},
-        {text: "워드프로세서1급", weight: 2},
-        {text: "Movie", weight: 2},
-        {text: "Blog", weight: 2},
-        {text: "PHOTO", weight: 2},
+		{text: "jQuery", weight: 13, url: ""},
+		{text: "JxYxxng", weight: 10.5, url: ""},
+		{text: "Parallax", weight: 9.4}, // url: "javascript:alert('JavaScript in URL is OK!');
+		{text: "HTML5", weight: 8},
+		{text: "CSS3", weight: 6.2},
+		{text: "API", weight: 5},
+		{text: "VanillaJS", weight: 5},
+		{text: "SASS", weight: 5},
+		{text: "반응형(Responsive Web)", weight: 5},
+		{text: "모바일", weight: 4},
+		{text: "PlugIn", weight: 4},
+		{text: "웹표준", weight: 3},
+		{text: "웹접근성", weight: 3},
+		{text: "의사소통", weight: 3},
+		{text: "웹디자인기능사", weight: 3},
+		{text: "워드프로세서1급", weight: 2},
+		{text: "자기개발", weight: 2},
+		{text: "수학과", weight: 2},
+		{text: "Movie", weight: 2},
+		{text: "Blog", weight: 2},
+		{text: "PHOTO", weight: 2},
 		{text: "Web Publisher", weight: 2},
 		{text: "빠른속도", weight: 2},
-        {text: "웹표준", weight: 2},
-        {text: "웹접근성", weight: 2},
-        {text: "크로스 브라우징", weight: 2},
-        {text: "Zeplin", weight: 1},
-        {text: "Photoshop", weight: 1},
-        {text: "Illustrator", weight: 1},
-        {text: "Eclipse", weight: 1},
-        {text: "VScode", weight: 1},
-        {text: "GitHub", weight: 1},
-        {text: "JxYxxng", weight: 1}
-      ];
-      $(document).ready(function() {
-        $("#my_favorite_latin_words").jQCloud(word_list);
-      });
+		{text: "꼼꼼함", weight: 2},
+		{text: "성실함", weight: 2},
+		{text: "책임감", weight: 2},
+		{text: "크로스 브라우징", weight: 2},
+		{text: "Zeplin", weight: 1},
+		{text: "Photoshop", weight: 1},
+		{text: "Illustrator", weight: 1},
+		{text: "Eclipse", weight: 1},
+		{text: "VScode", weight: 1},
+		{text: "GitHub", weight: 1}
+		];
+	$(document).ready(function() {
+		$("#my_favorite_latin_words").jQCloud(word_list);
+	});
+	  
+
+	// // 카카오맵
+	var container = document.getElementById('map');
+	var options = {
+		center: new kakao.maps.LatLng(37.510257, 126.946595), // 중심좌표
+		level: 3 // 지도 크기
+	};
+	var map = new kakao.maps.Map(container, options);
+	var markerPosition  = new kakao.maps.LatLng(37.510257, 126.946595); // 마커 위치
+	var marker = new kakao.maps.Marker({ // 마커 생성
+		position: markerPosition
+	});
+	marker.setMap(map); // 마커 표시
 });
