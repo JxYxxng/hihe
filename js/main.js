2891448710b1a8171bbfc92743123487d86dcf15
JxYxxng <ajy9798@gmail.com>
2020-02-27 20:58:27 +0900

    지도 마커 추가

--- js/main.js
+++ js/main.js
@@ -75,13 +75,17 @@ $(function(){
 	});
 
 	// 카카오맵
-
-	var container = document.getElementById('map'); //지도를 담을 영역의 DOM 레퍼런스
-	var options = { //지도를 생성할 때 필요한 기본 옵션
-		center: new kakao.maps.LatLng(37.510257, 126.946595), //지도의 중심좌표.
-		level: 3 //지도의 레벨(확대, 축소 정도)
+	var container = document.getElementById('map');
+	var options = {
+		center: new kakao.maps.LatLng(37.510257, 126.946595), // 중심좌표
+		level: 3 // 지도 크기
 	};
-	var map = new kakao.maps.Map(container, options); //지도 생성 및 객체 리턴
+	var map = new kakao.maps.Map(container, options);
+	var markerPosition  = new kakao.maps.LatLng(37.510257, 126.946595); // 마커 위치
+	var marker = new kakao.maps.Marker({ // 마커 생성
+		position: markerPosition
+	});
+	marker.setMap(map); // 마커 표시
 
 	// 포트폴리오
 	$(".portBtn").click(function(e){
